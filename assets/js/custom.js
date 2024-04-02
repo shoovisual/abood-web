338c3740da9c0291945e21f3f13f12745f549e31
shoovisual <shoovisual@gmail.com>
2024-04-02 23:28:48 +0300

    Files added

---
+++ assets/js/custom.js
@@ -0,0 +1,120 @@
+/* =============================================================================
+-----------------------------  Button scroll up   ------------------------------
+============================================================================= */
+
+$(document).ready(function () {
+    "use strict";
+
+    var progressPath = document.querySelector(".progress-wrap path");
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition = "stroke-dashoffset 10ms linear";
+    var updateProgress = function () {
+        var scroll = $(window).scrollTop();
+        var height = $(document).height() - $(window).height();
+        var progress = pathLength - (scroll * pathLength) / height;
+        progressPath.style.strokeDashoffset = progress;
+    };
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 150;
+    var duration = 550;
+    jQuery(window).on("scroll", function () {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery(".progress-wrap").addClass("active-progress");
+        } else {
+            jQuery(".progress-wrap").removeClass("active-progress");
+        }
+    });
+    jQuery(".progress-wrap").on("click", function (event) {
+        event.preventDefault();
+        jQuery("html, body").animate({ scrollTop: 0 }, duration);
+        return false;
+    });
+});
+
+// logo-slide
+$(document).ready(function () {
+    $(".client-slider").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        lazyLoad: "ondemand",
+        dots: false,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 820,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
+    });
+});
+
+// logo-slide
+$(document).ready(function () {
+    $(".bus-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        lazyLoad: "ondemand",
+        dots: false,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
+    });
+});
+// logo-slide
+$(document).ready(function () {
+    $(".team-slider").slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        arrows: false,
+        lazyLoad: "ondemand",
+        dots: true,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 820,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+});
